5244b5ed6b6cd9dad1c1d7452dd329787e82b524
inmiti <inmabauvel@gmail.com>
2023-09-28 23:30:06 +0200

    Creadas vistas herores con tableView y detail

--- Superheroes/Heroes/HeroesViewController.swift
+++ Superheroes/Heroes/HeroesViewController.swift
@@ -9,22 +9,52 @@ import UIKit
 
 class HeroesViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let countries: [String] = ["España", "Francia", "Portugal"] //Aquí iría el listado con los heróes
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("listado de heroes")
+        title = "Table"  // Titulo de viewController
+        tableView.dataSource = self  // ponemos un dataSource que es self
+        tableView.delegate = self  // ponemos undelegate que es self
+        tableView.alwaysBounceVertical = true // desactiva el efecto de hacer scroll cuando no hay mas filas
+    
+        
+//        guard let url = URL(string: "https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg")else {
+//            return
+//        }
+//        imageView.setImage(for: url)
 
-        // Do any additional setup after loading the view.
+        
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// - MARK: - Table View Datasource
+extension HeroesViewController: UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+            return countries.count  // cuenta el numero de filas según el listado de héroes
     }
-    */
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell() // Al no hacer una celda custom podemos hacerlo así
+        let country = countries[indexPath.row] // pasamos el index de nuestra fila
+        cell.textLabel?.text = country //Pasamos el nombre del país al que representamos
+        return cell
+    }
+}
 
+// MARK: - Table View Delegate
+extension HeroesViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath) {
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
--- Superheroes/Login/LoginViewController.swift
+++ Superheroes/Login/LoginViewController.swift
@@ -12,12 +12,18 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var imageLoginView: UIImageView!
+    
     // - MARK: Private let:
     private let model = NetworkModel()  // inicializamos la clase
     
     // - MARK: LiveCycle:
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let url = URL(string: "https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg")else {
+            return
+        }
+        imageLoginView.setImage(for: url)
     }
     
     // - MARK: Actions:
@@ -31,32 +37,33 @@ class LoginViewController: UIViewController {
                 print("Token: \(token)")
                 DispatchQueue.main.async {
                     let heroesviewController = HeroesViewController() // inicializamos nuestro viewController
-                    self?.navigationController?.showDetailViewController(heroesviewController, sender: nil)  //metodo show para navegar al listado de Heroes
+                    self?.navigationController?.setViewControllers([heroesviewController], animated: true)   //metodo show para navegar al listado de Heroes
+
                 }
                 // En el controlador de vista de inicio de sesión
-                self?.model.getHeroes { result in      // No hacer esto en la práctica revisar lo que puedo quitar
-                    switch result {
-                        case let .success(heroes):
-                            print("Heroes: \(heroes)")
-
-                            let goku = heroes[3]
-                            self?.model.getTransformations(
-                                for: goku
-                            ) { result in
-                                switch result {
-                                    case let .success(transformations):
-                                        print("Transformations: \(transformations)")
-                                    case let .failure(error):
-                                        print("Error: \(error)")
-
-                                    }
-                                }
-
-                        case let .failure(error):
-                            print("Error: \(error)")
-
-                    }
-                }
+//                self?.model.getHeroes { result in      // No hacer esto en la práctica revisar lo que puedo quitar
+//                    switch result {
+//                        case let .success(heroes):
+//                            print("Heroes: \(heroes)")
+//
+//                            let goku = heroes[3]
+//                            self?.model.getTransformations(
+//                                for: goku
+//                            ) { result in
+//                                switch result {
+//                                    case let .success(transformations):
+//                                        print("Transformations: \(transformations)")
+//                                    case let .failure(error):
+//                                        print("Error: \(error)")
+//
+//                                    }
+//                                }
+//
+//                        case let .failure(error):
+//                            print("Error: \(error)")
+//
+//                    }
+//                }
             case let .failure(error):
                 print("Error: \(error)")
             }
